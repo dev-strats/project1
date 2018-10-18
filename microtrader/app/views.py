a396e38187a92b1a140654b3b5009a5415f84e97
DESKTOP-PFPVRNL\litao <litao.ext@gmail.com>
2018-10-19 00:28:04 +0100

    Add strategy.html to search and present strategy details

--- microtrader/app/views.py
+++ microtrader/app/views.py
@@ -1,8 +1,6 @@
 from flask import render_template, flash, redirect, jsonify 
 from app import app
 import pandas as pd
-from .models.rolling_future_strategy import RollingFutureStrategy
-from .models.vol_target import VolTarget
 from .models.tradable_base import TradableManager
 from .utils import converter
 
@@ -11,36 +9,21 @@ from .utils import converter
 def index():
     return 'Hello world!'
 
-@app.route('/future_data/<start_date>/<end_date>')
-def rolling_future_data(start_date, end_date):
-    # this function should be retired one the internal tradable API is wired into the webpage
-    start_date = pd.to_datetime(start_date)
-    end_date = pd.to_datetime(end_date)
-
-    rf = RollingFutureStrategy("Rolling Future 001", "ES", "QUANDL", "CME/ESH2013", start_date)
-    values = rf.get_values_json(start_date, end_date)
-    result = {
-            "properties" : rf.get_properties(),
-            "param_data" : rf.get_param_data(),
-            "values"     : converter.covert_price_to_gdata(values)
-    }
-    return jsonify(result)
-
-@app.route('/internal/load_all_tradables')
+# Tao: should let TradableManager do such work internally,
+#      so will remove this function in future
 def load_all_tradables():
     # a dummy function to return some tradables
     # this will be replaced by proper load of real positions
+
+    from .models.rolling_future_strategy import RollingFutureStrategy
+    from .models.vol_target import VolTarget
+
     rf = RollingFutureStrategy("Rolling_Future_001", "ES", "QUANDL", "CME/ESH2013", pd.to_datetime("2013-04-15"))
     vt = VolTarget("Vol_Target_on_Rolling_Future_001", "Rolling_Future_001", 0.8, 0.2, pd.to_datetime("2013-04-15"))
-    return jsonify({})
-
-@app.route('/internal/tradable_names_by_type')
-def tradable_names_by_type():
-    ret = TradableManager.get_all_tradable_names_by_types()
-    return jsonify(ret)
 
 @app.route('/internal/strategy_names_by_type')
 def strategy_names_by_type():
+    load_all_tradables()
     ret = TradableManager.get_all_strategy_names_by_types()
     return jsonify(ret)
 
@@ -56,6 +39,6 @@ def tradable_data(tradable_name, start_date, end_date):
 
     return jsonify(json_data)
 
-@app.route('/future')
-def rolling():
-    return render_template('future.html')
+@app.route('/strategy')
+def strategy():
+    return render_template('strategy.html')
